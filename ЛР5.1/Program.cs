7cf55d06e9c089c0b9bc4b2366da8b130f1642ac
MatveyBaulin <baulinm2006@gmail.com>
2024-11-18 10:40:58 +0300

    Добавьте файлы проекта.

---
+++ ЛР5.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {   // Задаём длину массива
+        Console.WriteLine("Введите длину массива:");
+        int n = int.Parse(Console.ReadLine());
+
+        // Создаём массив
+        int[] array = new int[n];
+
+        //Осуществляем ввод с клавиатуры
+        Console.WriteLine("Введите элементы массива:");
+
+        int maxElement = 0;
+        int index = 0;
+
+        //Находим среди введённых элементов максимальный элемент(и его индекс)
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            array[i] = num;
+
+            if (num > maxElement)
+            {
+                maxElement = num;
+                index = i;
+            }
+        }
+
+        // Проверка на заполненность массива(для исключения результата при 0 длине)
+        if (n > 0)
+        {
+            Console.WriteLine($"Наибольший элемент массива: {maxElement}");
+            Console.WriteLine($"Порядковый номер элемента: {index}");
+        }
+        else
+        {
+            Console.WriteLine("Массив пустой");
+        }
+    }
+}
